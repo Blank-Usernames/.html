921b44a532c9ea005dc640619de2bd55af968035
Eduardo Santos <eduardo.soliveira67@senacsp.edu.br>
2025-05-13 11:48:49 -0300

    Exercício 14 - Criação

---
+++ atividades/JS/script014.js
@@ -0,0 +1,16 @@
+function passouMouse() {
+    document.querySelector('.button').innerHTML = "<p>Passou o mouse</p>"
+}
+
+var count = 0
+var number = 1
+
+function clicouMouse() {
+    count += 1
+    number.innerHTML = count
+    document.querySelector('.button').innerHTML = "<p>Clicou com o mouse (</p>"+count+"<p>) vezes!</p>"
+}
+
+function tirouMouse() {
+    document.querySelector('.button').innerHTML = "<p>Tirou o mouse</p>"
+}
