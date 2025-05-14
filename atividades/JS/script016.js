e8eb77a65105b9bbddb187ca687897dd16631d9c
Eduardo Santos <eduardo.soliveira67@senacsp.edu.br>
2025-05-14 11:40:13 -0300

    Exercício 16 - Update Fix

--- atividades/JS/script016.js
+++ atividades/JS/script016.js
@@ -3,5 +3,6 @@ function mostrarMidia() {
     let inMusica = document.querySelector('#inputMusica').value
     let inJogo = document.querySelector('#inputJogo').value
 
+    document.querySelector('#mostrar').style.display = "block"
     document.querySelector('#mostrar').innerHTML = "<p><strong>Filme: </strong>"+inFilme+"</p><p><strong>Música: </strong>"+inMusica+"</p><p><strong>Jogo: </strong>"+inJogo+"</p>"
 }
