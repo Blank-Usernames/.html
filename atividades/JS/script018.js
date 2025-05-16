cf5344fc3e17fafc1f1d7dbd750afce8176747b8
Eduardo Santos <eduardo.soliveira67@senacsp.edu.br>
2025-05-16 08:46:00 -0300

    Exercício 18 - Conclusão

--- atividades/JS/script018.js
+++ atividades/JS/script018.js
@@ -1,9 +1,9 @@
 function inserirNome() {
-    document.querySelector('#displayName').style.display = "block"
     inputNomes = document.querySelector('#inputNome').value
 
     let nomeUsuarios = document.createElement('p')
     nomeUsuarios.innerHTML = inputNomes
-    document.displayName.prepend(nomeUsuarios)
+    document.querySelector('#submitName').appendChild(nomeUsuarios);
 
+    document.querySelector('#inputNome').value = ""
 }
